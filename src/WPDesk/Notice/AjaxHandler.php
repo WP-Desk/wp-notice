c18ea4cfea250a70ba8af7ed04a9c48ffa7e5c4a
Grzegorz Rola <grola@seostudio.pl>
2023-03-03 09:17:32 +0100

    feature(ajax): added nonce

--- src/WPDesk/Notice/AjaxHandler.php
+++ src/WPDesk/Notice/AjaxHandler.php
@@ -12,13 +12,13 @@ use WPDesk\PluginBuilder\Plugin\PluginAccess;
  *
  * @package WPDesk\Notice
  */
-class AjaxHandler implements HookablePluginDependant
-{
+class AjaxHandler implements HookablePluginDependant {
 
     use PluginAccess;
 
     const POST_FIELD_NOTICE_NAME = 'notice_name';
     const POST_FIELD_SOURCE = 'source';
+    const POST_FIELD_SECURITY = 'security';
 
     const SCRIPTS_VERSION = '4';
     const SCRIPT_HANDLE = 'wpdesk_notice';
@@ -33,44 +33,39 @@ class AjaxHandler implements HookablePluginDependant
      *
      * @param string|null $assetsURL Assets URL.
      */
-    public function __construct($assetsURL = null)
-    {
+    public function __construct( $assetsURL = null ) {
         $this->assetsURL = $assetsURL;
     }
 
     /**
      * Hooks.
      */
-    public function hooks()
-    {
-        if ($this->assetsURL) {
-            add_action('admin_enqueue_scripts', [$this, 'enqueueAdminScripts']);
+    public function hooks() {
+        if ( $this->assetsURL ) {
+            add_action( 'admin_enqueue_scripts', [ $this, 'enqueueAdminScripts' ] );
         } else {
-            add_action('admin_head', [$this,'addScriptToAdminHead']);
+            add_action( 'admin_head', [ $this, 'addScriptToAdminHead' ] );
         }
-        add_action('wp_ajax_wpdesk_notice_dismiss', [$this, 'processAjaxNoticeDismiss']);
+        add_action( 'wp_ajax_wpdesk_notice_dismiss', [ $this, 'processAjaxNoticeDismiss' ] );
     }
 
     /**
      * Enqueue admin scripts.
      */
-    public function enqueueAdminScripts()
-    {
-        $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+    public function enqueueAdminScripts() {
         wp_register_script(
             self::SCRIPT_HANDLE,
-            trailingslashit($this->assetsURL) . 'js/notice' . $suffix . '.js',
-            array( 'jquery' ),
+            trailingslashit( $this->assetsURL ) . 'js/notice.js',
+            [ 'jquery' ],
             self::SCRIPTS_VERSION
         );
-        wp_enqueue_script(self::SCRIPT_HANDLE);
+        wp_enqueue_script( self::SCRIPT_HANDLE );
     }
 
     /**
      * Add Java Script to admin header.
      */
-    public function addScriptToAdminHead()
-    {
+    public function addScriptToAdminHead() {
         include __DIR__ . '/views/admin-head-js.php';
     }
 
@@ -79,24 +74,29 @@ class AjaxHandler implements HookablePluginDependant
      *
      * Updates corresponded WordPress option and fires wpdesk_notice_dismissed_notice action with notice name.
      */
-    public function processAjaxNoticeDismiss()
-    {
-        if (isset($_POST[self::POST_FIELD_NOTICE_NAME])) {
-            $noticeName = sanitize_text_field($_POST[self::POST_FIELD_NOTICE_NAME]);
+    public function processAjaxNoticeDismiss() {
+        if ( isset( $_POST[ self::POST_FIELD_NOTICE_NAME ] ) ) {
+            $noticeName = sanitize_text_field( $_POST[ self::POST_FIELD_NOTICE_NAME ] );
 
-            if (isset($_POST[self::POST_FIELD_SOURCE])) {
-                $source = sanitize_text_field($_POST[ self::POST_FIELD_SOURCE ]);
+            if ( isset( $_POST[ self::POST_FIELD_SOURCE ] ) ) {
+                $source = sanitize_text_field( $_POST[ self::POST_FIELD_SOURCE ] );
             } else {
                 $source = null;
             }
 
-            update_option(
-                PermanentDismissibleNotice::OPTION_NAME_PREFIX . $noticeName,
-                PermanentDismissibleNotice::OPTION_VALUE_DISMISSED
-            );
-            do_action('wpdesk_notice_dismissed_notice', $noticeName, $source);
+            $security = $_POST[ self::POST_FIELD_SECURITY ] ?? '';
+
+            $option_name = PermanentDismissibleNotice::OPTION_NAME_PREFIX . $noticeName;
+
+            if ( wp_verify_nonce( $security, $option_name ) ) {
+                update_option(
+                    $option_name,
+                    PermanentDismissibleNotice::OPTION_VALUE_DISMISSED
+                );
+                do_action( 'wpdesk_notice_dismissed_notice', $noticeName, $source );
+            }
         }
-        if (defined('DOING_AJAX') && DOING_AJAX) {
+        if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
             die();
         }
     }
